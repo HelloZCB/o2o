34a685ee05f89dcb625312100ae2c1dea4fbbba8
HelloZCB <469356261@qq.com>
2018-08-16 20:54:51 +0800

    商品添加 Controller定义

---
+++ src/main/java/com/zcb/o2o/web/shopadmin/ProductManagementController.java
@@ -0,0 +1,32 @@
+package com.zcb.o2o.web.shopadmin;
+
+import com.zcb.o2o.service.ProductCategoryService;
+import com.zcb.o2o.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+@Controller
+@RequestMapping("/shopadmin")
+public class ProductManagementController {
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ProductCategoryService productCategoryService;
+
+    private static final int IMAGEMAXCOUNT = 6;
+
+    @RequestMapping(value = "/addproduct", method = RequestMethod.POST)
+    @ResponseBody
+    private Map<String, Object> addProduct(HttpServletRequest request) {
+        Map<String, Object> modleMap = new HashMap<>();
+        return modleMap;
+    }
+}
