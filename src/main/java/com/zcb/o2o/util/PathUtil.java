bad4377b8f322c13fd9001f353c4250607174096
HelloZCB <469356261@qq.com>
2019-04-06 14:02:40 +0800

    完成商品列表前后端开发

--- src/main/java/com/zcb/o2o/util/PathUtil.java
+++ src/main/java/com/zcb/o2o/util/PathUtil.java
@@ -16,7 +16,7 @@ public class PathUtil {
     }
 
     public static String getShopImagePath(long shopId) {
-        String imagePath = "upload/item/shop/" + shopId + "/";
+        String imagePath = "/upload/item/shop/" + shopId + "/";
         return imagePath.replace("/", separator);
     }
 }
--- src/main/java/com/zcb/o2o/web/frontend/FrontendController.java
+++ src/main/java/com/zcb/o2o/web/frontend/FrontendController.java
@@ -7,8 +7,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/frontend")
 public class FrontendController {
+
+    /**
+     * 首页路由
+     *
+     * @return
+     */
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     private String index(){
         return "frontend/index";
+    }
+
+    /**
+     * 商品列表页路由
+     *
+     * @return
+     */
+    @RequestMapping(value = "/shoplist", method = RequestMethod.GET)
+    private String showShopList() {
+        return "frontend/shoplist";
     }
 }
